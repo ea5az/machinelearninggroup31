d7e2795f09b7f74d80f6fe1e2840fbf5f71f0e07
Jan Hendrik Kirchner <jkirchner@Jans-MacBook-Pro.local>
2015-05-07 19:51:37 +0200

    Yep, now it really works. Had to take the square root of the variance. Also made a nice picture as a proof.

--- sheet3/exc1_nice.m
+++ sheet3/exc1_nice.m
@@ -21,12 +21,14 @@ for i = 1:3
     alpha(i) = length(partition{i})/n;
 end
 
-eps = 0.001;
+eps = 0.05;
 N = 100;
 p = zeros(200 , 3);
 first_run = true;
 n = 1;
-mu_delta=0;sigma_delta=0;
+mu_delta=0;sigma_delta=0;old_mu=0;old_sigma=0;
+figure();
+n = 1;
 while((mu_delta > eps && sigma_delta > eps) || first_run)
     %Likelihoods unnormalized
     p(: , 1) = normpdf(data,mu(1),sigma(1))*alpha(1);
@@ -46,9 +48,9 @@ while((mu_delta > eps && sigma_delta > eps) || first_run)
     mu(2) = (1./summed_rows(2))*sum(data .* p(: , 2) , 1);
     mu(3) = (1./summed_rows(3))*sum(data .* p(: , 3) , 1);
     
-    sigma(1) = (1./summed_rows(1))*sum((data-mu(1)).^2 .* p(: , 1) , 1);
-    sigma(2) = (1./summed_rows(2))*sum((data-mu(2)).^2 .* p(: , 2) , 1);
-    sigma(3) = (1./summed_rows(3))*sum((data-mu(3)).^2 .* p(: , 3) , 1);
+    sigma(1) = sqrt((1./summed_rows(1))*sum((data-mu(1)).^2 .* p(: , 1) , 1));
+    sigma(2) = sqrt((1./summed_rows(2))*sum((data-mu(2)).^2 .* p(: , 2) , 1));
+    sigma(3) = sqrt((1./summed_rows(3))*sum((data-mu(3)).^2 .* p(: , 3) , 1));
 
     mean_p = mean(p , 1);
     
@@ -57,21 +59,37 @@ while((mu_delta > eps && sigma_delta > eps) || first_run)
     alpha(3) = mean_p(3);
     
     
-    old_mu = mu; old_sigma = sigma;
     mu_delta = sum(abs(mu-old_mu));
     sigma_delta = sum(abs(sigma-old_sigma));
-    [~,assign] = max(p , [] , 2)
+    old_mu = mu; old_sigma = sigma;
+
+    display(mu_delta)
+    display(sigma_delta)
+
+    [~,assign] = max(p , [] , 2);
     
-    part1 = data(assign == 1)
-    part2 = data(assign == 2)
-    part3 = data(assign == 3)
-%     hold on;
-%     dx = linspace(0,length(x) , length(x));
-%     plot(dx,part1,'o');
-%     plot(dx,part2,'o');
-%     plot(dx,part3,'o');
+    part1 = data(assign == 1);
+    part2 = data(assign == 2);
+    part3 = data(assign == 3);
     
-    first_run = true;
+    subplot(6,3,n);
+
+    hold on;
+    dx1 = zeros(length(part1)) ;
+    dx2 = zeros(length(part2)) ;
+    dx3 = zeros(length(part3)) ;
+    
+    
+    plot(part1,dx1,'o','color','red');
+    plot(part2,dx2,'o','color','blue');
+    plot(part3,dx3,'o','color','green');
+    for j =1:3
+        ix = linspace (-4.5 ,4 ,200);
+        plot(ix,normpdf(ix,mu(j),sigma(j))+1)
+    end
+    hold off;
+    n = n+1
+    first_run = false;
 end
 alpha
 mu
