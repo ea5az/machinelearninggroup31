0f7cebc4c333e0f1e7ddba0d31a1d7b1481339b2
Jan Kirchner <kirchner.jan@icloud.com>
2015-05-09 19:08:20 +0200

    Didn't actually solve the exercise the last time. But this time around I have a pretty good feeling. Like really. Code is fast as well.

---
+++ sheet3/ex2_second_try.m
@@ -0,0 +1,43 @@
+clear all; close all;
+load('points.mat');
+
+C = cell([200,1]);
+n = length(pts);
+for i = 1:n
+    C{i} = i;
+end
+%calculate complete distance matrix once
+distances = pdist2(pts,pts);
+distances(distances==0)=Inf;
+for i= n:-1:6
+    %get smallest distance
+    [~,ind] = min(distances(:));
+    [xentr,yentr] = ind2sub([n,n],ind);
+    C{min(xentr,yentr)} = [C{xentr},C{yentr}];
+    C{max(xentr,yentr)} = [];
+
+    %%% patching the matrix up for the next step
+    mergeInto = min(xentr,yentr);
+    mergeFrom = max(xentr,yentr);
+    
+    distances(mergeInto,:) = min([distances(xentr,:);distances(yentr,:)]);
+    distances(:,mergeInto) = min([distances(xentr,:);distances(yentr,:)]);
+    distances(mergeInto,mergeInto) = Inf;
+    distances(xentr,yentr) = Inf;
+    distances(yentr,xentr) = Inf;       
+    distances(mergeFrom,:) = Inf([200,1]);
+    distances(:,mergeFrom) = Inf([200,1]);
+end
+
+
+%%
+figure();
+hold on;
+for i = 1:n
+    if ~(isempty(C{i}))
+        plot(pts(C{i},1),pts(C{i},2),'o')
+    end
+end
+
+%high = max([distances(xentr,:);distances(yentr,:)])
+
