ddf65e205159b67ec5416300f455f95baa03f120
milsen <milsen@users.noreply.github.com>
2015-06-14 00:35:42 +0200

    Improve 7-3, it works
    But I have no idea why.
    The weights and bias converge, that's a good thing. Even all the errors
    go to 0. When I used a threshold of 0.5 and a normal plot for the
    decision boundary, however, it always looked off.
    Setting the threshold to 0.25 and negating the bias in the plot of the
    decision boundary leads to a correct categorization. But is this the
    proper way of doing it or this indirect hard coding of the solution?

--- sheet7/group31_sheet7_3.py
+++ sheet7/group31_sheet7_3.py
@@ -11,9 +11,10 @@ train_x = np.array(loadmat("train_or", matlab_compatible=True)['x'])
 train_t = np.array(loadmat("train_or", matlab_compatible=True)['t'])
 test_x = np.array(loadmat("test_or", matlab_compatible=True)['x_test'])
 
-print train_x.shape
-print train_t.shape
-print test_x.shape
+# use oldfaithful data instead if desired
+# train_x = np.array(loadmat("train_oldfaithful", matlab_compatible=True)['x'])
+# train_t = np.array(loadmat("train_oldfaithful", matlab_compatible=True)['t'])
+# test_x = np.array(loadmat("test_oldfaithful", matlab_compatible=True)['x_test'])
 
 # plot data
 plt.figure(figsize=(10,5))
@@ -23,56 +24,45 @@ plt.title("Noisy OR-data")
 plt.xlim(-2,2)
 plt.ylim(-2,2)
 
-# append x0 = [1,1] to use bias later as w0
-# train_x = np.append([[1,1]],train_x,axis=0)
-# train_t = np.append([1],train_t)
-
 # choose learning rate as well as threshold
-EPSILON = 0.01      # learning rate
-THETA = 0.5         # threshold
+EPSILON = 0.01     # learning rate
+THETA = 0.25        # threshold
 
 # define output activation function as given in exercise
 def output(y_in):
     return 1 if y_in > THETA else (-1 if y_in < -THETA else 0)
 
-# initialize as many random weights as there are data points
-w = np.random.rand(train_x.shape[0],train_x.shape[1])
+# initialize as many random weights as there are input units (2)
+w = np.random.rand(train_x.shape[1])
 
 # bias = w0
-bias = np.random.rand(train_x.shape[1])
+bias = np.random.randint(-1,1)
 
-error = np.ones(12)
-# while error.all() > 0:
-for j in range(2000):
+# initialize error, loop until it reduces to 0 (no sample is miscategorized)
+error = np.ones(train_x.shape[0])
+while error.any() != 0:
     for i in range(train_x.shape[0]):
-        # get output using activation function of activation = weights*data
-        y_in = np.dot(train_x[i],w[i].T)
-        y = output(y_in)
-        print y
 
-        # update weights and implicitly bias (=w0)
+        # get output using activation function
+        # (do not forget bias as w0)
+        y_in = np.dot(train_x[i],w) + bias
+        y = output(y_in)
+
+        # update weights
         delta_w = EPSILON*(train_t[i]-y)*train_x[i]
-        w[i] += delta_w
+        w += delta_w
 
         # update bias
         bias += EPSILON*(train_t[i]-y)
 
-    # for stop criterion, calculate error
-    # error = 0.5*sum((train_t - np.array(
-    #     [output(np.dot(train_x[i],w[i].T)) for i in
-    #         range(train_x.shape[0])]))**2)
-    # print error
+        # update error (how far away ouput is from training result)
+        error[i] = train_t[i]-y
 
-print w 
-print bias
+# plot decision boundary
+x = np.linspace(-2,2,500)
+plt.plot(x,(-bias - (w[0]/w[1])*x),'m-')
 
-plt.gca().quiver(bias[0],bias[1],w.T[0],w.T[1],angles='xy',scale_units='xy',scale=1)
-plt.show()
-
-# plot test data
+# plot test data as green xs
 for i in range(test_x.shape[0]):
-    plt.plot(test_x[i][0],test_x[i][1],'ro')#,"ro" if test_t[i] == 1 else "bo")
-plt.title("Data with Weights")
-# plt.xlim(-2,2)
-# plt.ylim(-2,2)
+    plt.plot(test_x[i][0],test_x[i][1],'gx')
 plt.show()
